b553ca41f36eacec73937f3a29a4dd554c9ce80c
Ondřej Pokorný <pokorny.ondrej@gmail.com>
2020-12-04 10:19:21 +0100

    day 4 part 2

--- 2020/day_04/day_04.clj
+++ 2020/day_04/day_04.clj
@@ -25,26 +25,50 @@
   (map #(if (and (contains? % key)
                  (fun (get % key))) 1 0) col))
 
-(def validation-set (hash-map "byr" #((println %) true)
-                     "iyr" #((println %) true)
-                     "eyr" #((println %) true)
-                     "hgt" #((println %) true)
-                     "hcl" #((println %) true)
-                     "ecl" #((println %) true)
-                     "pid" #((println %) true)))
+(defn validate-number [n min max]
+  (and (>= n min)
+       (<= n max)))
+
+(defn validate-height [height min-cm max-cm min-in max-in]
+  (let [[val units] [(subs height 0 (- (count height) 2))
+                      (subs height (- (count height) 2))]]
+    (cond (= units "cm") (validate-number (Integer/parseInt val) min-cm max-cm)
+          (= units "in") (validate-number (Integer/parseInt val) min-in max-in)
+          :else false)))
+
+(defn validate-color [c]
+  (not (nil? (re-matches #"#[0-9a-f]{6}" c))))
+
+(def eye-tbl #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"})
+(defn validate-eye [e]
+  (contains? eye-tbl e))
+
+(defn validate-pass-id [pass]
+  (not (nil? (re-matches #"[0-9]{9}" pass))))
+
+(def validation-set (hash-map
+                     "byr" (fn [w] (validate-number (Integer/parseInt w) 1920 2002))
+                     "iyr" (fn [w] (validate-number (Integer/parseInt w) 2010 2020))
+                     "eyr" (fn [w] (validate-number (Integer/parseInt w) 2020 2030))
+                     "hgt" (fn [w] (validate-height w 150 193 59 76))
+                     "hcl" (fn [w] (validate-color w))
+                     "ecl" (fn [w] (validate-eye w))
+                     "pid" (fn [w] (validate-pass-id w))))
 
 (def not-required "cid")
 
-(map (fn [[f s]] (println "first: " f "second " s)) validation-set)
+;; (map (fn [[f s]] (println "first: " f "second " s) "a") validation-set)
 
-;; (def valid-set-marked (map #(has-valid input (first %) (second %)) validation-set))
+(def valid-set-marked
+  (map (fn [[k v]] (has-valid input k v)) validation-set))
 
-;; (defn sum-lists [first second]
-;;   (map + first second))
+(defn sum-lists [first second]
+  (map + first second))
 
-;; (def valid-pass (apply + (reduce (fn [acc col]
-;;                                    (map #(if (= % 2) 1 0) (sum-lists acc col)))
-;;                                  (first valid-set-marked)
-;;                                  (rest valid-set-marked))))
+(def valid-pass (apply + (reduce (fn [acc col]
+                                   (map #(if (= % 2) 1 0) (sum-lists acc col)))
+                                 (first valid-set-marked)
+                                 (rest valid-set-marked))))
 
-;; (println "Part 1 result: " valid-pass)
+;; after changes to part 2; the Part 1 validation is too strict (remove validation functions)
+(println "Part 2 result: " valid-pass)
