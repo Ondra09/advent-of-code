aea0beb4d85ad2581934d96ba2075857f39befc8
Ondřej Pokorný <ondrej.pokorny@avast.com>
2020-12-06 12:47:22 +0100

    day 6 cleanup

--- 2020/day_06/day_06.clj
+++ 2020/day_06/day_06.clj
@@ -6,7 +6,7 @@
   (str/split s #"\n\n"))
 
 (defn parse-input [line]
-  (map #(set (seq (char-array %))) (str/split line #"\n")))
+  (map #(set %) (str/split line #"\n")))
 
 (defn modify-input [col]
   col)
@@ -17,12 +17,8 @@
                 (map #(parse-input %))
                 (map #(modify-input %))))
 
-(defn merge-groups [col]
-  (reduce (fn [acc val] (concat acc val))  col))
-
-(defn part-1 [input fun]
+(defn sum-result [input fun]
   (apply + (map #(count (apply fun %)) input)))
 
-(println "Part 1 result: " (part-1 input clojure.set/union))
-(println "Part 1 result: " (part-1 input clojure.set/intersection))
-
+(println "Part 1 result: " (sum-result input clojure.set/union))
+(println "Part 2 result: " (sum-result input clojure.set/intersection))
