0ceaba14c48b0659aeb40ef27c869d46cef25d24
Ondřej Pokorný <pokorny.ondrej@gmail.com>
2020-12-08 07:34:33 +0100

    day 7 first part

--- 2020/day_07/day_07.clj
+++ 2020/day_07/day_07.clj
@@ -1,60 +1,59 @@
 #!/usr/local/bin/bb
-(require '(clojure.test :refer [deftest is])
-         '(clojure.string :refer [includes?]))
+
+(ns main
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.string :as str]))
 ;; https://adventofcode.com/2020/day/7
 
 (defn split-input [s]
   (str/split s #"\n"))
 
 (defn parse-input [line]
-  (let [[_ name items] (re-matches #"([ \w]+) bags contain (.*)" line)]
-    {name items}))
-
-
-(defn modify-input [col]
-  (into {}[(first col) (rest col)]))
+  (let [[_ name items] (re-matches #"([ \w]+) bags contain (.*)" line)
+        children (re-seq #"(\d) ([ \w]+) bag" items)]
+    {name children}))
 
 (def input (->> (slurp "input-test")
                 (str/trim)
                 (split-input)
                 (map #(parse-input %))
-                ;(map #(modify-input %))
                 (into {})
                 ))
 
-
-
-(defn is-child? [col name]
-  (includes? (second col) name))
-
-(defn visited? [v col]
-  (some #(= % v) col))
+(defn get-parents [col name]
+  (->> col
+       (filter (fn [[k v]] (str/includes? v name)))
+       (map (fn [lst] (first lst)))))
 
 (defn find-all-parents [nodes node-name]
-  (loop [nodes nodes
-         node-names [node-name]
-         visited #{}]
-    (if (empty? node-names)
-      visited
-      (let [parents (filter #(is-child? % (first node-names)) input)
-            not-visited (filter #(not (visited? % visited)) parents)]
-        (recur nodes not-visited (conj visited node-name))
-        ))))
+  (loop [stack `(~node-name)
+         parent-names #{}]
+    (if (empty? stack)
+      parent-names
+      (let [parents (get-parents nodes (first stack))
+            new-stack (into (rest stack) parents)
+            new-parent-names (into parent-names parents)
+            ]
+        (recur new-stack new-parent-names)))))
 
-(filter #(is-child? % "shiny gold") input)
-
-
+(defn find-all-bags [nodes node-name]
+  (loop [stack `(~node-name)
+         sum 0]))
+input
+;(println "Part 1 result: " (count (find-all-parents input "shiny gold")))
+;(println "Part 2 result: " 1)
 ;;;;;;;;;;;;;;;;;;;;;; TESTS
-(deftest is-valid?
-  (is (= 5 5)))
+;; (deftest is-valid?
+;;   (is (= 5 5)))
 
-(defn run-tests []
-  (is-valid?))
+;; (defn run-tests []
+;;   (is-valid?))
 
 ; (run-tests)
 ; (filter #(is-child? % "shiny gold") input)
 
 ; ((hash-map "abc" "aaa") "abc")
 ; (find-all-parents input "shiny gold")
-;
-input
+
+;input
+;(find-all-parents input "shiny gold")
