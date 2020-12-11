77f9d64b940ca5acd8c17c6cd8a68d7adcbf55a9
Ondřej Pokorný <ondrej.pokorny@avast.com>
2020-12-11 21:30:19 +0100

    day 11 base

--- 2020/day_11/day_11.clj
+++ 2020/day_11/day_11.clj
@@ -5,8 +5,6 @@
             [clojure.string :as str]
             ))
 
-(load-file "../util/simple2D.clj")
-(clojure.core/alias 's2D 'util.simple2D)
 ;; https://adventofcode.com/2020/day/11
 
 (defn split-input [s]
@@ -18,16 +16,39 @@
 (def input (->> (slurp "input-test")
                 (str/trim)
                 (split-input)
-                (map #(parse-input %))
-                (map #(vec %))
-                (to-array-2d)))
+                (mapv #(parse-input %))
+                ))
 
 
-(defn get-neigh [coll])
+;; ----> X
+;; |
+;; |
+;; v
+;; Y
+
+(defn get-in-str [coll x y]
+  (get (get coll y) x))
+
+(def eight-neigbour [[-1 -1] [0 -1] [1 -1]
+                     [-1 0] [1 0]
+                     [-1 1] [0 1] [1 1]])
+
+(defn count-neighbors [coll x y value]
+  (reduce (fn [acc [dx dy]] (if (= value (get-in-str coll (+ dx x) (+ dy y)))
+                              (do (println "dx "dx " " dy " " acc " val " value " getin " (get-in-str coll (+ dx x) (+ dy y))) (inc acc))
+                              (do (println "dx "dx " " dy " " acc " val " value " getin " (get-in-str coll (+ dx x) (+ dy y))) 0))) 0 eight-neigbour))
 
 (println "Part 1 result: " 1)
 (println "Part 2 result: " 1)
-(println input)
+input
+
+(println "> " (get-in-str input 2 2))
+
+(println (count-neighbors input 1 1 \L))
+
+input
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;; TESTS
 ;; (deftest is-valid?
